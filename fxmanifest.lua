0a12a35866c2442c3c47266557db96431d4041ee
Armin <123939474+ss-armin@users.noreply.github.com>
2023-01-30 13:05:37 +0330

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+
+games { 'rdr3', 'gta5' }
+
+author 'Ss Armin'
+
+description 'To bring accidents closer to reality . . .'
+
+version '1.0.0'
+
+client_scripts {
+	'config.lua',
+	'client.lua'
+}
